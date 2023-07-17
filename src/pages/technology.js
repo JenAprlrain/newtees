f832a961c1d8132401bd1e8e31eede392f6af579
JenAprlrain <jenheidt1@gmail.com>
2023-07-17 15:42:17 -0400

    seperated paragraphs on technology page.

--- src/pages/technology.js
+++ src/pages/technology.js
@@ -29,9 +29,8 @@ return (
       </div>
       <h2 className="image-title">Technology</h2>
       <div>
-      <h2 className="WhatIsInfo">Base Web 3 Technology – From the beginning, OfficialNFTees has utilized QR codes to enhance the experience of owning one of our NFTs and physical replicas (the combination of which is often referred to as Phygitals). With the addition of our Web 3 Wardrobe ™, users can prove ownership of their NFTs through a simple QR scan.
-
-Series 1 Technology – through partnership with Authentic Vision, a world leader in authentication labels, and their Meta Anchor technology, OfficialNFTees has added a layer of security to our physicals. Meta Anchors are a serialized, 1/1 holographic QR labels that are not replicable, even by the machines that created them. This technology adds a layer of verification of authenticity that precedes the proof of ownership over the NFTee in the Web3 Wardrobe ™.</h2>
+      <h2 className="WhatIsInfo">Base Web 3 Technology – From the beginning, OfficialNFTees has utilized QR codes to enhance the experience of owning one of our NFTs and physical replicas (the combination of which is often referred to as Phygitals). With the addition of our Web 3 Wardrobe ™, users can prove ownership of their NFTs through a simple QR scan.</h2>
+      <h2 className="WhatIsInfo">Series 1 Technology – through partnership with Authentic Vision, a world leader in authentication labels, and their Meta Anchor technology, OfficialNFTees has added a layer of security to our physicals. Meta Anchors are a serialized, 1/1 holographic QR labels that are not replicable, even by the machines that created them. This technology adds a layer of verification of authenticity that precedes the proof of ownership over the NFTee in the Web3 Wardrobe ™.</h2>
       </div>
       <br></br>
       <br></br>
